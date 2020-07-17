f681baa80e4032a322c76b5501895aba3680801d
James Baker <james.baker@15gifts.com>
2020-07-17 11:37:37 +0100

    PROD-216 - Use global-safe polyfills
    Add browserlist config
    Add webpack config

---
+++ webpack.babel.config.js
@@ -0,0 +1,42 @@
+const webpack = require('webpack')
+const { resolve } = require('path')
+const nodeExternals = require('webpack-node-externals')
+
+module.exports = {
+  mode: 'production',
+  entry: resolve(__dirname, 'src/index.js'),
+  externals: [nodeExternals()], // in order to exclude all node_modules from bundle
+  output: {
+    publicPath: '/',
+    path: resolve(__dirname, 'build'),
+    filename: 'index.js',
+    library: 'reduxWiretap',
+    libraryTarget: 'umd',
+  },
+  resolve: {
+    extensions: ['.js', '.jsx'],
+    modules: [resolve(__dirname, 'src')],
+  },
+  devtool: 'sourceMap',
+  module: {
+    rules: [
+      {
+        test: /.jsx?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            // For some reason the babelrc isn't picked up
+            configFile: resolve(__dirname, '.babelrc'),
+          },
+        },
+      },
+    ]
+  },
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV),
+      'process.env.DEBUG': JSON.stringify(process.env.DEBUG),
+    }),
+  ],
+}
