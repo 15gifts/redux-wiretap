730dc0f71258bc9515d4591a6267a37f9656b40a
James Baker <james.baker@15gifts.com>
2020-07-17 12:03:55 +0100

    PROD-216 - Use commonjs2 to bundle

--- webpack.babel.config.js
+++ webpack.babel.config.js
@@ -11,7 +11,7 @@ module.exports = {
     path: resolve(__dirname, 'build'),
     filename: 'index.js',
     library: 'reduxWiretap',
-    libraryTarget: 'umd',
+    libraryTarget: 'commonjs2',
   },
   resolve: {
     extensions: ['.js', '.jsx'],
